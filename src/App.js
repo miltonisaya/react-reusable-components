1e03ef0a0fcf8b50e8c28c232473bc7e64560611
Milton Isaya <miltonisaiah@gmail.com>
2023-05-09 11:43:54 +0300

    app component added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import React from 'react';
 import './App.css';
-import UserComponent from "./modules/user/home";
+import CustomTable from "./components/Table";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import Button from "./components/Button";
+import CustomModal from "./components/Modal";
+import {Grid} from "@mui/material";
 
-function App() {
-    const columns = [
-        {field:'id', headerName: 'Id'},
-        {field:'firstName', headerName: 'First Name'},
-        {field:'lastName', headerName: 'Last Name'},
-        {field:'fullName', headerName: 'Full Name', type: 'number'},
-        {field:'role', headerName: 'Role'}
-    ];
+const API = "https://jsonplaceholder.typicode.com/users";
 
-    const rows = [];
+const getPosts = async ()=>{
+    return await axios.get(API);
+}
+
+
+const columns = [
+    {title: "Name", value: "name"},
+    {title: "Username", value: "username"},
+    {title: "Email", value: "email"},
+    {title: "Actions", value: "actions"},
+];
+
+const App = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [posts,setPosts] = useState([]);
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    const fetchData = async ()=>{
+        const res = await getPosts();
+        setPosts(res.data)
+    }
+
+    const openDialog = () => {
+        console.log('open dialog');
+        setIsOpen(true);
+    }
+
+    const closeDialog = () => {
+        setIsOpen(false)
+    }
+
+
     return (
-        <UserComponent columns={columns} rows={rows}/>
+        <Grid container sx={{padding: '40px'}}>
+            <Grid item xs={12} sx={{paddingBottom: '10px'}}>
+                <Button onClick={openDialog}>Add New</Button>
+            </Grid>
+            <Grid item>
+                <CustomTable columns={columns} data={posts}>
+                    {({row, col}) => (
+                        <button>Edit</button>
+                    )}
+                </CustomTable>
+            </Grid>
+
+            <CustomModal open={isOpen} onClose={closeDialog} title='User Form'>
+                Form
+            </CustomModal>
+        </Grid>
     );
 }
 
